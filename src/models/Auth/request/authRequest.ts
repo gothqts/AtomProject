fde1f04deb746d043baa95ec45c81689f89895e9
Lev <yarken121@gmail.com>
2024-11-18 21:30:51 +0500

    fix auth types

--- src/models/Auth/request/authRequest.ts
+++ src/models/Auth/request/authRequest.ts
@@ -1,2 +1,12 @@
-export interface RegisterRequest {}
-export interface LoginRequest {}
+export interface RegisterRequest {
+  fio: string
+  email: string
+  password: string
+  confirmPassword: string
+  city: string
+  status: string
+}
+export interface LoginRequest {
+  email: string
+  password: string
+}
--- src/models/Auth/response/authResponse.ts
+++ src/models/Auth/response/authResponse.ts
@@ -1,3 +1,13 @@
-export interface AuthResponse {}
+export interface AuthResponse {
+  completed: boolean
+  status: string
+  message: string
+  userId: string
+  accessToken: string
+}
 
-export interface BaseStatusResponse {}
+export interface BaseStatusResponse {
+  completed: boolean
+  status: string
+  message: string
+}
