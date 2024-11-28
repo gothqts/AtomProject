1d936ce1ac59b1849f7c3e177382da1bcb6e80d6
StarsFaLL7 <starsfall-ru@yandex.ru>
2024-11-28 19:41:07 +0500

    Add: эндпоинты для просмотра, изменения и удаления события Add: эндпоинты для изменения баннера события

---
+++ Backend/UnbeatableBookingSystem/Controllers/Events/Requests/UpdateEventRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace UnbeatableBookingSystem.Controllers.Events.Requests;
+
+public class UpdateEventRequest
+{
+    public required bool IsPublic { get; set; }
+    
+    public required string Title { get; set; }
+    
+    public required bool IsOnline { get; set; }
+    
+    public required DateTime DateStart { get; set; }
+    
+    public required DateTime DateEnd { get; set; }
+    
+    public string? City { get; set; } = null!;
+
+    public string? Address { get; set; } = null!;
+    
+    public required bool IsSignupOpened { get; set; }
+
+    public string Description { get; set; } = null!;
+}
---
+++ Backend/UnbeatableBookingSystem/Controllers/Events/Responses/EventResponse.cs
@@ -0,0 +1,36 @@
+﻿using UnbeatableBookingSystem.Controllers.UserActions.Responses;
+
+namespace UnbeatableBookingSystem.Controllers.Events.Responses;
+
+public class EventResponse
+{
+    public Guid Id { get; set; }
+    
+    public DateTime CreationDate { get; set; }
+    
+    public required bool IsPublic { get; set; }
+    
+    public required string Title { get; set; }
+
+    public required string BannerImage { get; set; }
+    
+    public required bool IsOnline { get; set; }
+    
+    public required bool IsSignupOpened { get; set; }
+
+    public string? City { get; set; } = null!;
+
+    public string? Address { get; set; } = null!;
+    
+    public required DateTime DateStart { get; set; }
+    
+    public required DateTime DateEnd { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public required SignupWindowResponse[] SignupWindows { get; set; }
+    
+    public required EventFormResponse SignupForm { get; set; }
+    
+    public required ContactsResponse[] Contacts { get; set; }
+}
